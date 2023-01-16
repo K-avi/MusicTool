4724c7d352f8d63d245d294bc7e0c4153828137a
K-avi <i.m-r.ecole@orange.fr>
2023-01-16 22:47:30 +0100

    modified example smtool file

--- examples/smtool_ex1.sh
+++ examples/smtool_ex1.sh
@@ -6,9 +6,12 @@
 ./smtool -scale "intv vector { 0 2 4 5 7 9 11}"
 ./smtool -scale "nearby { 0 1 2 3 5 6 8}"
 
+
 ./smtool -chprog "toscale [II, V,I]"
 ./smtool -chprog "rand"
 
 ./smtool -read "examples/command_ex3.txt"
 #being able to read the commands in a Mtool file from smtool might be the most 
 #significant feature 
+
+#this script leaks a lot of memory but the commands don't on their own; I just kinda suck at bash ig
